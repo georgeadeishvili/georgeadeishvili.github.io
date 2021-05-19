3305d5e94d7a5dc7c9e34831a43792dc891e3068
georgeadeishvili <giorgi.adeishvili@gmail.com>
2021-05-19 12:53:08 +0400

    before ejecting

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { ThemeProvider, DefaultTheme, createGlobalStyle } from 'styled-components'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+const theme: DefaultTheme = {
+	borderRadius: '8px',
+	colors: {
+		primary: '#000',
+		secondary: '#fff',
+	},
+}
+
+const GlobalStyles = createGlobalStyle`
+	*, *::after, *::before {
+		margin: 0;
+		padding: 0;
+		box-sizing: inherit;
+	}
+	html {
+		font-size: 62.5%;
+	}
+	body {
+		box-sizing: border-box;
+		font-family: Poppins, sans-serif;
+		font-weight: 400;
+		line-height: 1.7;
+		color: ${({ theme }) => theme.colors.primary};
+	}
+`
+
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<App />
+			<GlobalStyles />
+		</ThemeProvider>
 	</React.StrictMode>,
 	document.getElementById('root'),
 )
